14b1d93ae11347ac96f5e3592508edebe663289d
anbul <62352932+321032100@users.noreply.github.com>
2022-01-10 02:20:48 +0800

    Update docker-entrypoint.sh

--- docker/files/cgi/docker-entrypoint.sh
+++ docker/files/cgi/docker-entrypoint.sh
@@ -4,9 +4,5 @@
 /usr/sbin/init
 /usr/sbin/crond
 echo "info: starting php-fpm.."
-sed -i '/display_errors/d' /etc/php.ini
-sed -i '/error_reporting/d' /etc/php.ini
-echo "error_reporting  =  E_ALL">>/etc/php.ini
-echo "display_errors = Off">>/etc/php.ini
 /usr/sbin/php-fpm -c /etc/php.ini -y /etc/php-fpm.conf
 tail -f /dev/null
